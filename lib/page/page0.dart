1e4c3e88567567a9aa011d1ed3028b9c909ebc97
TaNSiRa <tansirawit2543@gmail.com>
2025-07-08 10:17:07 +0700

    .

--- lib/page/page0.dart
+++ lib/page/page0.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'TEST.dart';
-import 'page10.dart';
+import 'page1.dart';
 
 bool isChecked = false;
 
@@ -10,7 +10,7 @@ class Page0 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Page10();
+    return Page1();
   }
 }
 
